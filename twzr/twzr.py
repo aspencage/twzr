173486c45ea2a1e7d198cf40fe9aa5a6cfa33878
Aspen Cage <aspenncage@gmail.com>
2023-08-28 12:56:48 -0700

    time string and read csv wrappers

--- twzr/twzr.py
+++ twzr/twzr.py
@@ -5,10 +5,30 @@ import numpy as np
 import os 
 import time  
 import re 
+from typing import Union
 
+
+# read csv
+def rc(fn,fd=None,fe=None,chdir:bool=True):
+    if fe is not None:
+        fn = fn+"."+fe
+    if fd is not None:
+        if chdir:
+            os.chdir(fd)
+        else:
+            fn = os.path.join(fd,fn)
+    return pd.read_csv(fn)
+
+
+# filter 
 f = lambda x,y: x.filter(regex=re.compile(y,re.IGNORECASE))
+
+
+# value count
 vc = lambda x,y,d=False,n=True: x[y].value_counts(dropna=d,normalize=n)
 
+
+# weighted value count
 def wvc(df,c,n=True):
     gb = df.groupby([c]).sum()
     if n:
@@ -50,57 +70,57 @@ def ch_vals_from(df,c,ovs:list,nvs:list):
         ch_val_from(df,c,ov,nv)
 
 
-def xt(df,row_col,col_col,w="auto_weight",normalize="index"):
+def xt(df,row_col,col_col,w=None,n="index"):
     if w is not None:
         return pd.crosstab(
             df[row_col],
             df[col_col],
             df[w],
             aggfunc="sum",
-            normalize=normalize
+            normalize=n
             )
     else:
         return pd.crosstab(
             df[row_col],
             df[col_col],
-            normalize=normalize
+            normalize=n
             )
 
 
-def move_col(df,col,new_index=None):
-    # TODO allow for string name
-    # defaults to last column 
+def move_col(df,col:Union[str,int],new_index:int=None):
+    if isinstance(col,int):
+        col = df.columns[col]
     if new_index is None:
         new_index = df.shape[1]-1
     xs = df.pop(col)
     df.insert(new_index,col,xs)
 
+
+def ts():
+    return time.strftime("_%Y%m%d-%H%M")
+
+
 help_str = """
     current functions are:
-        f
-        vc
-        wvc
-        cross
-        cross_col
-        ch_val
-        ch_vals
-        ch_val_from
-        ch_vals_from
-        xt
-        move_col
+        rc              pd.read_csv() with options for file directory (fd) and file extension (fe). if fd provided, chdir=True will change working directory.
+        f               df.filter() wrapper function, case-insensitive. arguments: x: pd.DataFrame, y:str (column name)
+        vc              df.value_counts() wrapper function. arguments: x: pd.DataFrame, y: str (column name), d=False:bool (dropna), n=True: bool (normalize)
+        wvc             weighted value counts
+        xt              pd.crosstab() wrapper function that readily handles weights. arguments: df,row_col,col_col,w=None:str (weight column),n="index":str (normalize argument)
+        cross           cross two strings
+        cross_col       cross two columns of strings and create a new column that is the variable names crossed
+        ch_val          wrapper for numpy.where with a boolean mask, new value, dataframe, and given column
+        ch_vals         for loop over `ch_val` with lists of masks and new values for a given column
+        ch_val_from     wrapper for numpy.where with a (set of) previous values in place of a boolean mask
+        ch_vals_from    for loop over `ch_val_from` with lists of old values and new values for a given column
+        move_col        move a column. arguments: df, col:str|int (column label or index),new_index:int (desired column index)
+        ts              generate a string with the current time
+        """
 
-    
-"""
+def help(print_=True,return_=False):
+    if print_:
+        print(help_str)
+    if return_:
+        return(help_str)
 
 
-def help():
-
-if __name__ == "__main__":
-    
-    fd = r""
-    os.chdir(fd)
-    fn = ""
-
-    df = pd.read_csv(fn)
-
-    ts = time.strftime("_%Y%m%d-%H%M")
