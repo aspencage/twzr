39210b40fad756f124571fa4a84ec4c33c1f6572
Aspen Cage <aspenncage@gmail.com>
2023-08-28 14:20:18 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import setuptools
 
 setuptools.setup(
     name='twzr',
-    version='0.1.6',
+    version='0.1.7',
     author='Aspen Cage',
     author_email='aspenncage@gmail.com',
     description='Tweezer: teeny tiny tools for data processing',
     long_description="Microfunctions to help make pandas data transformation workflows faster. For example, Why type `df.filter(regex=re.compile('column',re.IGNORECASE))` when you can type `f(df,'column')`?",
     long_description_content_type="text/markdown",
     url='https://github.com/aspencage/twzr',
-    download_url='https://github.com/aspencage/twzr/archive/refs/tags/v0.1.6.tar.gz',
+    download_url='https://github.com/aspencage/twzr/archive/refs/tags/v0.1.7.tar.gz',
     license='MIT',
     packages=['twzr'],
     install_requires=[
